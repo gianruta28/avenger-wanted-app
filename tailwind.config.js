48e61dd715ee9ff32ef177f484b2b94d44cf4cb3
Gianfranco <gianjrs.98@gmail.com>
2023-03-31 21:10:56 +0200

    Added Spinner gif and changed body background, also fixed things in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  purge: {
-    enabled: true,
-    content: [
+  content: [
     './src/**/*.html',
     './src/**/*.scss'],
-  },
   theme: {
     extend: {},
   },
