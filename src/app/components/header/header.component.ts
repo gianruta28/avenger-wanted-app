28b2e235a0808a454722344c740d6cd0e6e5f850
Gianfranco <gianjrs.98@gmail.com>
2023-04-02 01:34:36 +0200

    Log out button and some enhancements on the comic scrolls for mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { filter } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import { HandleSessionService } from 'src/app/services/handle-session.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   showBackButton: boolean = false;
-  constructor(private router: Router) {
+  agent: any;
+  constructor(private router: Router, private sessionManager: HandleSessionService) {
     router.events.subscribe(event => {
       if (event instanceof NavigationStart){
         if(event.url !== '/' && event.url !== '/auth') this.showBackButton = true;
@@ -18,10 +19,33 @@ export class HeaderComponent {
      }
 
     });
-};
+    
+  }
 
-goHomePage(){
-  this.router.navigate(['/']);
-}
+  ngOnInit(){
+    
+    this.sessionManager.sessionAction$.subscribe((value) => {
+      console.log(value);
+      
+      if(value instanceof Object){
+        this.agent = '';
+      }else {
+        this.agent = value;
+      }
+    })
+    
+    this.agent = JSON.parse(this.sessionManager.getSession()).name;
+    console.log(this.agent.name);
+    
+  }
+
+  goHomePage(){
+    this.router.navigate(['/']);
+  }
+
+  logout(){
+    this.sessionManager.deleteSession();
+    this.router.navigate(['/auth'])
+  }
 
 }
